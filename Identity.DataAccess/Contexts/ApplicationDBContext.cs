617b6b3f764e0c161900abb8be27e50ed5f1a33e
Hector Luis <hector.duran@tarmac.io>
2023-04-13 22:02:19 -0400

    Add project files.

---
+++ Identity.DataAccess/Contexts/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.DataAccess.Contexts
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+            
+        }
+    }
+}
---
+++ Identity.Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool Isactive { get; set; } = false;
+    }
+}
